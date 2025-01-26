db2aa7ff77aecf4886fe56a0a60cd42be8c7fa1f
Teofilo Jr. Daquipil <contact@lemonypancakes.me>
2025-01-26 18:25:39 +0800

    Update resourcemanagerhelper to v1.4.1

--- src/main/java/me/lemonypancakes/races/power/PowerRepository.java
+++ src/main/java/me/lemonypancakes/races/power/PowerRepository.java
@@ -1,7 +1,17 @@
 package me.lemonypancakes.races.power;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import me.lemonypancakes.resourcemanagerhelper.Resource;
+import me.lemonypancakes.resourcemanagerhelper.ResourceLocation;
+import me.lemonypancakes.resourcemanagerhelper.ResourceManagerHelper;
 import org.bukkit.NamespacedKey;
 
+import javax.naming.Name;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,7 +23,25 @@ public final class PowerRepository {
     }
 
     public PowerRepository reload() {
-        //Map<ResourceLocation, Resource> resources = ResourceManagerHelper.listResources("power", s -> s.toString().endsWith(".json"));
+        Map<ResourceLocation, Resource> resources = ResourceManagerHelper.listResources("powers", s -> s.toString().endsWith(".json"));
+        Gson gson = new Gson();
+
+        resources.forEach((resourceLocation, resource) -> {
+            try (InputStream inputStream = resource.open()) {
+                Reader reader = new InputStreamReader(inputStream);
+                JsonObject json = gson.fromJson(reader, JsonObject.class);
+                NamespacedKey key = NamespacedKey.fromString(json.get("type").getAsString());
+                PowerType<?> type = PowerType.get(key);
+
+                if (type == null) return;
+                PowerFactory<?> factory = type.factory();
+                Power power = factory.create(key, json);
+
+                powers.put(key, power);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
         return this;
     }
 

b238e7fbd7a1f61005078af1772a3d21bfad5136
Teofilo Jr. Daquipil <me@lemonypancakes.me>
2025-02-19 14:17:29 +0800

    Refactor condition classes to use DataTypes for consistency and rename InvertedCondition to NotCondition

--- src/main/java/me/lemonypancakes/races/condition/NotCondition.java
+++ src/main/java/me/lemonypancakes/races/condition/NotCondition.java
@@ -4,18 +4,19 @@ import java.util.Collections;
 import java.util.List;
 import me.lemonypancakes.races.serialization.DataSchema;
 import me.lemonypancakes.races.serialization.DataType;
+import me.lemonypancakes.races.serialization.DataTypes;
 
-public final class InvertedCondition<T> extends Condition<T> {
+public final class NotCondition<T> extends Condition<T> {
   private final List<Condition<T>> conditions;
 
-  public InvertedCondition(List<Condition<T>> conditions) {
+  public NotCondition(List<Condition<T>> conditions) {
     this.conditions = conditions;
   }
 
   public static <T> ConditionFactory<T> getFactory(DataType<T> type) {
     return new ConditionFactory<>(
-        new DataSchema().add("conditions", DataType.listOf(type)),
-        container -> new InvertedCondition<>(container.get("conditions")));
+        new DataSchema().add("conditions", DataTypes.listOf(type)),
+        container -> new NotCondition<>(container.get("conditions")));
   }
 
   public List<Condition<T>> getConditions() {
--- src/main/java/me/lemonypancakes/races/condition/OrCondition.java
+++ src/main/java/me/lemonypancakes/races/condition/OrCondition.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.List;
 import me.lemonypancakes.races.serialization.DataSchema;
 import me.lemonypancakes.races.serialization.DataType;
+import me.lemonypancakes.races.serialization.DataTypes;
 
 public final class OrCondition<T> extends Condition<T> {
   private final List<Condition<T>> conditions;
@@ -14,7 +15,7 @@ public final class OrCondition<T> extends Condition<T> {
 
   public static <T> ConditionFactory<T> getFactory(DataType<T> type) {
     return new ConditionFactory<>(
-        new DataSchema().add("conditions", DataType.listOf(type)),
+        new DataSchema().add("conditions", DataTypes.listOf(type)),
         container -> new OrCondition<>(container.get("conditions")));
   }
 

40ae10602baf46d3c44062953c1023cddff95581
Teofilo Jr. Daquipil <me@lemonypancakes.me>
2025-03-06 16:41:19 +0800

    Format deserialize method in UserDataDeserializer for improved readability

--- src/main/java/me/lemonypancakes/races/user/UserDataDeserializer.java
+++ src/main/java/me/lemonypancakes/races/user/UserDataDeserializer.java
@@ -1,17 +1,17 @@
 package me.lemonypancakes.races.user;
 
 import com.google.gson.*;
-
 import java.lang.reflect.Type;
 import java.util.UUID;
 
 public final class UserDataDeserializer implements JsonDeserializer<User> {
-    @Override
-    public User deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        JsonObject obj = json.getAsJsonObject();
-        UUID uuid = UUID.fromString(obj.get("uuid").getAsString());
-        boolean hasRaceBefore = obj.get("hasRaceBefore").getAsBoolean();
+  @Override
+  public User deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+      throws JsonParseException {
+    JsonObject obj = json.getAsJsonObject();
+    UUID uuid = UUID.fromString(obj.get("uuid").getAsString());
+    boolean hasRaceBefore = obj.get("hasRaceBefore").getAsBoolean();
 
-        return null;
-    }
+    return null;
+  }
 }
